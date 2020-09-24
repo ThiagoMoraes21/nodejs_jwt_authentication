4ddacc54472bd9186c36ca277efea807c6235031
thiagoMoraes21 <thiago4x70@gmail.com>
2020-09-23 21:42:33 -0300

    add passport local strategy to do user authentication

--- src/usuarios/usuarios-rotas.js
+++ src/usuarios/usuarios-rotas.js
@@ -1,10 +1,17 @@
 const usuariosControlador = require('./usuarios-controlador');
+const passport = require('passport');
 
 module.exports = app => {
-  app
-    .route('/usuario')
-    .post(usuariosControlador.adiciona)
-    .get(usuariosControlador.lista);
+	app
+		.route('/usuario/login')
+		.post(
+			passport.authenticate('local', { session: false }),
+			usuariosControlador.login)
 
-  app.route('/usuario/:id').delete(usuariosControlador.deleta);
+	app
+		.route('/usuario')
+		.post(usuariosControlador.adiciona)
+		.get(usuariosControlador.lista);
+
+	app.route('/usuario/:id').delete(usuariosControlador.deleta);
 };
